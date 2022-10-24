3dcaeb2720980a627429b6b3ece466c676384f6e
Bassam-devAndroid <bassamouf@std.mans.edu.eg>
2022-10-24 19:25:38 +0200

    new commit

--- src/main/kotlin/Code.kt
+++ src/main/kotlin/Code.kt
@@ -5,99 +5,62 @@ fun main()
     val reader = Scanner(System.`in`)
     print("Enter Your ID : ")
     val idInput = reader.nextInt()
-    val id = ID()
-    id.print(idInput)
+    val id = ID(idInput)
+    id.print()
+
 }
 
 
-class ID():DR()
+class ID(var id:Int):DR()
 {
-    var listOfName = listOf<String>("Ahmed","Amer","Anas","Belal","Hussein")
-    val listOfSubjects = listOf<String>("NetWorking  100","CS  100","Logic Programming 50")
-
-    fun print(id:Int)
+    fun name(id: Int)
     {
-        if (id == 1)
-        {
-            println("Name:"+listOfName[0])
-            println("id : 1")
-            println("Subjects :")
-            for (i in listOfSubjects)
-            {
-                println(i)
-            }
-            println("DR And Days: ")
-            for (j in listOfDr)
-            {
-                println("Prof "+ j)
-            }
-
+        println("Name:")
+        when(id){
+            1 -> println("Ahmed")
+            2 -> println("Amer")
+            3 -> println("Anas")
+            4 -> println("Belal")
+            5 -> println("Hussein")
         }
-        if (id == 2)
-        {
-            println("Name:"+listOfName[1])
-            println("id : 2")
-            println("Subjects :")
-            for (i in listOfSubjects)
-            {
-                println(i)
-            }
-            println("DR And Days: ")
-            for (j in listOfDr)
-            {
-                println("Prof "+ j)
-            }
 
+    }
+    fun material(id: Int)
+    {
+        println("Material:")
+        when(id){
+            1 -> println("CS")
+            2 -> println("Networking")
+            3 -> println("Math")
+            4 -> println("CS")
+            5 -> println("DS")
         }
-        if (id == 3)
-        {
-            println("Name:"+listOfName[2])
-            println("id : 3")
-            println("Subjects :")
-            for (i in listOfSubjects)
-            {
-                println(i)
-            }
-            println("DR And Days: ")
-            for (j in listOfDr)
-            {
-                println("Prof "+ j)
-            }
 
+    }
+    fun materialDegrees(id: Int)
+    {
+        println("Degree :")
+        when(id){
+            1 -> println("100")
+            2 -> println("50")
+            3 -> println("20")
+            4 -> println("100")
+            5 -> println("50")
         }
-        if (id == 4)
-        {
-            println("Name:"+listOfName[3])
-            println("id : 4")
-            println("Subjects :")
-            for (i in listOfSubjects)
-            {
-                println(i)
-            }
-            println("DR And Days: ")
-            for (j in listOfDr)
-            {
-                println("Prof "+ j)
-            }
 
-        }
-        if (id == 5)
-        {
-            println("Name:"+listOfName[4])
-            println("id : 5")
-            println("Subjects :")
-            for (i in listOfSubjects)
-            {
-                println(i)
-            }
-            println("DR And Days: ")
-            for (j in listOfDr)
-            {
-                println("Prof "+ j)
-            }
+    }
+    fun print()
+    {
+        name(id)
+        material(id)
+        materialDegrees(id)
+        nameDr(id)
+        DaysOfDr(id)
+    }
 
-        }
-        
+    fun print(function:()->Unit)
+    {
+        function()
     }
 
 }
@@ -105,7 +68,50 @@ class ID():DR()
 
 open class DR()
 {
-    val listOfDr = listOf<String>("Adel Saturday","Ebrahim Sunday","Samir Monday","Khaled Tuesday")
+    fun nameDr(id: Int)
+    {
+        println("Prof Name:")
+        when(id){
+
+            1 -> println("Prof Adel")
+            2 -> println("Prof Omar")
+            3 -> println("Prof Khaled")
+            4 -> println("Prof Adel")
+            5 -> println("Prof Ehab")
+        }
+
+    }
+    fun DaysOfDr(id: Int)
+    {
+        println("Days:")
+        when(id){
+            1 ->
+            {
+                println("Sunday")
+                println("Tuesday")
+            }
+            2 ->
+            {
+                println("Saturday")
+                println("Monday")
+            }
+            3 ->
+            {
+                println("Wednesday")
+                println("Thursday")
+            }
+            4 ->
+            {
+                println("Sunday")
+                println("Tuesday")
+            }
+            5 ->
+            {
+                println("Monday")
+                println("Tuesday")
+            }
+        }
+    }
 }
 
 
